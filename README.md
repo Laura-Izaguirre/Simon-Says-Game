4f7eec176be939142fc6929a5d28ef9718fe720a
Laura-Izaguirre <114610611+Laura-Izaguirre@users.noreply.github.com>
2023-01-30 23:33:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vanilla-js-template
+This is a template to work with Vanilla Javascript.
---
+++ js/main.js
@@ -0,0 +1,8 @@
+import '../assets/styles/style.scss';
+document.querySelector('#app').innerHTML = `
+<div>
+  <p>Esta es mi plantilla para Vanilla Javascript hecha con Vite</p>
+</div>
+`
+
+setupCounter(document.querySelector('#counter'))
