f6a7ce52941b1c9b5b9a7e342a686634629acfae
Laura Izaguirre <laurar.izaguirre@gmail.com>
2023-02-03 01:28:09 -0600

    Create HTML structure

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
 import '../assets/styles/style.scss';
-document.querySelector('#app').innerHTML = `
-<div>
-  <p>Esta es mi plantilla para Vanilla Javascript hecha con Vite</p>
-</div>
-`
 
-setupCounter(document.querySelector('#counter'))
